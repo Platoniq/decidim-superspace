6d38df62946a66bc85b0b5cee33aa531550b864d
Harrison Hassig <harrisonhassig@gmail.com>
2025-08-25 02:34:01 -0700

    Upgrade to decidim 0.30 (#39)
    * Upgrade to decidim 0.30 compatability
    
    * Upgrade steps
    
    ---------
    
    Co-authored-by: Fran Bolívar <francisco.bolivar@nazaries.com>

--- lib/decidim/superspaces/version.rb
+++ lib/decidim/superspaces/version.rb
@@ -3,8 +3,8 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Superspaces
-    VERSION = "0.2.0"
-    DECIDIM_VERSION = "0.29.2"
-    COMPAT_DECIDIM_VERSION = [">= 0.29.0", "< 0.30"].freeze
+    VERSION = "0.3.0"
+    DECIDIM_VERSION = "0.30.0"
+    COMPAT_DECIDIM_VERSION = [">= 0.30.0", "< 0.31"].freeze
   end
 end
