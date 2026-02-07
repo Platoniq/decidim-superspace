ae8524d06990c478d6e5488199513ca48dc7e436
Harrison Hassig <harrisonhassig@gmail.com>
2026-02-07 06:53:10 +0100

    Standard 0.31 update steps

--- spec/lib/decidim/superspaces/version_spec.rb
+++ spec/lib/decidim/superspaces/version_spec.rb
@@ -11,11 +11,11 @@ module Decidim
     end
 
     it "has decidim version" do
-      expect(subject::DECIDIM_VERSION).to eq("0.30.0")
+      expect(subject::DECIDIM_VERSION).to eq("0.31.0")
     end
 
     it "has decidim compatible version" do
-      expect(subject::COMPAT_DECIDIM_VERSION).to eq([">= 0.30.0", "< 0.31"])
+      expect(subject::COMPAT_DECIDIM_VERSION).to eq([">= 0.31.0", "< 0.32"])
     end
   end
 end
