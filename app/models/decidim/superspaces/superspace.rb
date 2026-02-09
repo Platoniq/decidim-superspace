1fb8a68d6abc695b4223245429109bc7fe3bccdf
Harrison Hassig <harrisonhassig@gmail.com>
2026-02-09 15:04:12 +0100

    Initiatives in model and migrations in test.yml

--- app/models/decidim/superspaces/superspace.rb
+++ app/models/decidim/superspaces/superspace.rb
@@ -39,6 +39,10 @@ module Decidim
         find_spaces_by_type("Decidim::Conference")
       end
 
+      def initiatives
+        find_spaces_by_type("Decidim::Initiative")
+      end
+
       def statistics(stats = nil)
         return Decidim::Superspaces::SuperspaceStatsPresenter.new(self).collection if stats.nil?
 
